4f53c195f5bac474a5a4d3f320dd3fdcb60a1fbc
Jozef Zitt <84540150+Dodkes@users.noreply.github.com>
2022-09-26 14:50:59 +0200

    Updated + animation

--- autoTypedText.js
+++ autoTypedText.js
@@ -1,22 +1,37 @@
 const automatedTextElement = document.getElementById('automatedText')
 const elementTypeElement = document.querySelectorAll('.elementType')
-const speed = 100
+const speed = 250
+let timeout
 
 elementTypeElement[0].textContent = '<h1>'
 elementTypeElement[1].textContent = '</h1>'
 
-const automatedText = 'React developer bruh'
+const automatedText = 'xZitt - React developer bruh'
 
 let splitText = automatedText.split('')
 let finalText = ''
 let index = 0
 
 
-const timeout = setInterval(typingFunction, speed)
 
 
 function typingFunction () {
-    automatedTextElement.textContent = finalText += splitText[index]
+
+    let element = document.createElement('p')
+    automatedTextElement.appendChild(element)
+    element.textContent = splitText[index]
+    element.style.display = 'none'
+    element.className = 'autoTypingParagraph'
+    $(element).fadeIn(400)
     index++
-    if (index == splitText.length) clearTimeout(timeout)
+    if (index == splitText.length) clearTimeout(timeout) 
 }
+
+
+const tag = document.getElementById('backgroundTag')
+tag.textContent = '</>'
+
+$(document).ready(() => {
+     timeout = setInterval(typingFunction, speed)
+     $(tag).fadeIn(1000)
+})
