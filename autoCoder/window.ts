17ded4cdadf240824f0f0b9c78a19095e4cb6df4
Jozef Zitt <84540150+Dodkes@users.noreply.github.com>
2022-10-04 20:08:29 +0200

    Add files via upload

--- autoCoder/window.ts
+++ autoCoder/window.ts
@@ -8,8 +8,21 @@ const typescriptContainer = document.querySelector<HTMLDivElement>('#typescript'
 const typescriptTextElement = document.querySelector<HTMLDivElement>('#typescriptText')
 const dot = document.querySelector<HTMLDivElement>('.dot')
 const tsTextContent = document.querySelector<HTMLDivElement>('.textContent')
+const cursorOne = document.querySelector<HTMLDivElement>('.cursor-1')
 
-const typingSpeed: number = 200
+const equalElement = document.querySelector<HTMLDivElement>('.equal-element')
+const typescriptTextContent = document.querySelector<HTMLDivElement>('.typescriptTextContent')
+//SCSS elements
+const scssContainer = document.querySelector<HTMLDivElement>('#scss')
+const scssElementOne = document.querySelector<HTMLDivElement>('#scss-autotype-1')
+const scssElementTwo = document.querySelector<HTMLDivElement>('#scss-autotype-2')
+
+const colon = document.querySelector<HTMLDivElement>('.colon')
+const comma = document.querySelector<HTMLDivElement>('.comma')
+//Heading element
+const outputHeading = document.querySelector('#output-heading')
+
+const typingSpeed: number = 100
 const transitionSpeed: number = 500
 let typescriptInterval
 let index: number = 0
@@ -18,10 +31,18 @@ const text: string = 'h1#heading'
 const splitText: string[] = text.split('')
 let finalText: string = ''
 //TS TEXT
-const tsText: string = 'heading'
+const tsText: string = 'headingtextContent'
 const splitTsText: string[] = tsText.split('')
-console.log(splitTsText)
+//TS TEXT CONTENT
+const tsTextContentText: string = "'I love typescript <3'"
+const splitTsTextContentText: string[] = tsTextContentText.split('')
+const realtsTextContentText: string = tsTextContentText.slice(1, tsTextContentText.length - 1)
+//SCSS TEXT CONTENT
+const scssTextContentOne: string = 'background-color'
+const splitScssTextContentOne: string[] = scssTextContentOne.split('')
 
+const scssTextContentTwo: string = ' $myColor'
+const splitScssTextContentTwo: string[] = scssTextContentTwo.split('')
 
 function typingFunction () {
         if (heading){
@@ -37,6 +58,7 @@ function typingFunction () {
 const interval = setInterval(typingFunction, typingSpeed)
 
 function typeDiv () {
+    if (cursorOne) cursorOne.style.display = 'none'
     if (heading) heading.textContent = ''
     if (divTextElement) divTextElement.style.display = 'inline'
     if (divTextElementClosing) divTextElementClosing.style.display = 'inline'
@@ -56,11 +78,69 @@ function typingTypescript () {
     index++
     if (index == 7) {
         if (dot) dot.style.cssText = 'display: inline;'
-        console.log('enough')
         if (tsTextContent){
             tsTextContent.style.display = 'inline'
         }
-    } else if (index == 8) {
+    } else if (index > 7) {
+        if (finalText === 'heading') {finalText = ''; index-- }
+        if (tsTextContent) tsTextContent.textContent = finalText += splitTsText[index]
+        if (finalText === 'textContent') { 
+            clearInterval(typescriptInterval) 
+            setTimeout(() => { 
+                if (equalElement) equalElement.style.display = 'inline'
+            typescriptInterval = setInterval(typescriptTextContentFunction, typingSpeed)
+
+            }, typingSpeed)
+            finalText = ''
+            index = 0
+        }
+    }
+}
+
+function typescriptTextContentFunction () {
+    if (index < splitTsTextContentText.length) {
+        finalText += splitTsTextContentText[index]
+        if (typescriptTextContent){
+            typescriptTextContent.style.display = 'inline'
+            typescriptTextContent.textContent = finalText
+        }
+        index++
+    } else {
+        if (outputHeading) outputHeading.textContent = realtsTextContentText
+        clearInterval(typescriptInterval)
+        setTimeout(scssPrepare, transitionSpeed)
+    }
+}
+
+function scssPrepare () {
+    index = 0
+    finalText = ''
+    if (typescriptContainer) typescriptContainer.style.display = 'none'
+    if (scssContainer) scssContainer.style.display = 'block'
+    typescriptInterval = setInterval(scssTyping, typingSpeed)
+}
+
+function scssTyping () {
+    if (scssElementOne && index < splitScssTextContentOne.length) {
+        scssElementOne.style.display = 'inline'
+        scssElementOne.textContent = finalText += splitScssTextContentOne[index]
+        index++
+    } else if (index == splitScssTextContentOne.length) {
+        if (colon) colon.style.display = 'inline'
+        clearInterval(typescriptInterval)
+        typescriptInterval = setInterval(scssSecondTyping, typingSpeed)
+        index = 0
+        finalText = ''
+    }
+}
+
+function scssSecondTyping () {
+    if (scssElementTwo && index < splitScssTextContentTwo.length){
+        scssElementTwo.style.display = 'inline'
+        scssElementTwo.textContent = finalText += splitScssTextContentTwo[index]
+        index++
+    } else {
+        if (comma) comma.style.display = 'inline'
         clearInterval(typescriptInterval)
     }
 }
