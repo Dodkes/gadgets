776a61857d31bda4fb397f0939ef10a31b058ef2
Jozef Zitt <84540150+Dodkes@users.noreply.github.com>
2022-10-14 22:31:45 +0200

    3D grid + 3D text
    - added also 3D text with shadow and turning animation

--- 3D grid/script.ts
+++ 3D grid/script.ts
@@ -1,33 +1,15 @@
 let time: number = 1000
 let differenceTime: number = 50
+let iteration: number = 26
 
 $(function() {
     setTimeout(() => { showTiles('.unit-1') }, 1000);
-    setTimeout(() => { showTiles('.unit-2') }, delay ());
-    setTimeout(() => { showTiles('.unit-3') }, delay ());
-    setTimeout(() => { showTiles('.unit-4') }, delay ());
-    setTimeout(() => { showTiles('.unit-5') }, delay ());
-    setTimeout(() => { showTiles('.unit-6') }, delay ());
-    setTimeout(() => { showTiles('.unit-7') }, delay ());
-    setTimeout(() => { showTiles('.unit-8') }, delay ());
-    setTimeout(() => { showTiles('.unit-9') }, delay ());
-    setTimeout(() => { showTiles('.unit-10') }, delay ());
-    setTimeout(() => { showTiles('.unit-11') }, delay ());
-    setTimeout(() => { showTiles('.unit-12') }, delay ());
-    setTimeout(() => { showTiles('.unit-13') }, delay ());
-    setTimeout(() => { showTiles('.unit-14') }, delay ());
-    setTimeout(() => { showTiles('.unit-15') }, delay ());
-    setTimeout(() => { showTiles('.unit-16') }, delay ());
-    setTimeout(() => { showTiles('.unit-17') }, delay ());
-    setTimeout(() => { showTiles('.unit-18') }, delay ());
-    setTimeout(() => { showTiles('.unit-19') }, delay ());
-    setTimeout(() => { showTiles('.unit-20') }, delay ());
-    setTimeout(() => { showTiles('.unit-21') }, delay ());
-    setTimeout(() => { showTiles('.unit-22') }, delay ());
-    setTimeout(() => { showTiles('.unit-23') }, delay ());
-    setTimeout(() => { showTiles('.unit-24') }, delay ());
-    setTimeout(() => { showTiles('.unit-25') }, delay ());
+    for (let x = 1; x < iteration; x++) {
+        setTimeout(() => { showTiles(`.unit-${x}`) }, delay());
+    }
 });
+
+console.log(document.querySelector(`.unit-${iteration}`))
 
 function showTiles (id: string) {
     $(id).css('visibility', 'visible')
